44fa5746fc96846a7e7957563160be9e2d4a737e
Nicholas Birkbeck <nbirkbeck@gmail.com>
2022-03-18 11:21:56 +1000

    Added updatePost

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -4,26 +4,40 @@ import { Resolver, Query, Ctx, Arg, Mutation } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
-  @Query(() => [Post]) // Query object type Post 
+  @Query(() => [Post]) // Query object type Post
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
   } // Graphql query 'posts' will return all results for type Post
 
   @Query(() => Post, { nullable: true })
-  post(
-    @Arg("id") id: number,
-    @Ctx() { em }: MyContext
-  ): Promise<Post | null> {
+  post(@Arg("id") id: number, @Ctx() { em }: MyContext): Promise<Post | null> {
     return em.findOne(Post, { id });
   } // Graphql query 'post' accepts an id arg and will return either null Or the result for matching id of type Post
-  
+
   @Mutation(() => Post)
   async createPost(
     @Arg("title") title: string,
     @Ctx() { em }: MyContext
-  ): Promise<Post | null> {
+  ): Promise<Post> {
     const post = em.create(Post, { title });
     await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post)
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (!post) {
+      return null;
+    }
+    if (typeof title !== "undefined") {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
     return post;
   }
 }
