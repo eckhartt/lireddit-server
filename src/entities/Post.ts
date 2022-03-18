9edf95cd50dfa8d33ed926ad29f699c3bd627f3c
Nicholas Birkbeck <nbirkbeck@gmail.com>
2022-03-18 11:22:23 +1000

    Set createdAt and updatedAt to be optional fields

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,9 +1,12 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, OptionalProps, PrimaryKey, Property } from "@mikro-orm/core";
 import { Field, ObjectType } from "type-graphql";
 
 @ObjectType()
 @Entity()
 export class Post {
+
+  [OptionalProps]?: 'createdAt' | 'updatedAt'; // Only 'title' will be required for `em.create()`. id is an optional prop by default. 
+
   @Field()
   @PrimaryKey({ type: "number" })
   id!: number;
