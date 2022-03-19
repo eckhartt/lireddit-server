681fb8e59bf9fec75dc11dfbbfc8db1d01dbbfd9
Nicholas Birkbeck <nbirkbeck@gmail.com>
2022-03-19 10:12:29 +1000

    Resolved session type err by declaration merging

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,16 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core"
+import { Request, Response } from 'express'
+//@ts-ignore
+import { Session } from 'express-session';
 
 export type MyContext = {
     em: EntityManager<IDatabaseDriver<Connection>>
+    req: Request
+    res: Response
+}
+
+declare module 'express-session' {
+ interface Session {
+    userId: number;
+  }
 }
