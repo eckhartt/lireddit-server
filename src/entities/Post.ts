eb8742007a2bd79f8ddc4be06c9e1b74e60027b6
Nicholas Birkbeck <nbirkbeck@gmail.com>
2022-04-07 18:56:17 +1000

    Created User-post relationship in entity

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -4,17 +4,38 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { User } from "./User";
 
 @ObjectType()
 @Entity()
 // Extending BaseEntity to give access to Post.find, Post.insert etc.
 export class Post extends BaseEntity {
   @Field()
-  @PrimaryGeneratedColumn('increment')
+  @PrimaryGeneratedColumn()
   id!: number;
+
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: "int", default: 0})
+  points!: number;
+
+  @Field()
+  @Column()
+  creatorId!: number;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  creator!: User;
 
   @Field(() => String)
   @CreateDateColumn()
@@ -23,8 +44,4 @@ export class Post extends BaseEntity {
   @Field(() => String)
   @UpdateDateColumn()
   updatedAt!: Date;
-
-  @Field()
-  @Column()
-  title!: string;
 }
